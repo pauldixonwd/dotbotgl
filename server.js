38e6ed53cdc2b765da0506b15c993e7784bedd3b
pauldixonwd <paul@pauldixonwebdesign.co.uk>
2017-01-29 22:10:42 +0000

    Update server.js

--- server.js
+++ server.js
@@ -31,10 +31,15 @@ I will respond to the following messages:
 
 
 // response to the user typing "bacon"
-slapp
-  .message('^(bacon)$', ['direct_mention', 'direct_message'], (msg, text) => {
-    msg
-.say(`${text}, yummy!`)
+slapp.message(/^(bacon|sausages)/i, ['mention', 'direct_message'], (msg) => {
+  // You can provide a list of responses, and a random one will be chosen
+  // You can also include slack emoji in your responses
+  msg.say([
+    "You're welcome :smile:",
+    'You bet',
+    ':+1: Of course',
+    'Anytime :sun_with_face: :full_moon_with_face:'
+  ])
 })
 
 // response to the user typing "help"
