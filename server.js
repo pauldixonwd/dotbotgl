a18929ad16e8c40e5b0026b727b4d6442d0cc4b5
pauldixonwd <paul@pauldixonwebdesign.co.uk>
2017-01-29 22:24:40 +0000

    Update server.js

--- server.js
+++ server.js
@@ -91,6 +91,18 @@ slapp.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
   ])
 })
 
+// Can use a regex as well
+slapp.message(/^(bacon|sausage)/i, ['mention', 'direct_message'], (msg) => {
+  // You can provide a list of responses, and a random one will be chosen
+  // You can also include slack emoji in your responses
+  msg.say([
+    "You're welcome :smile:",
+    'You bet',
+    ':+1: Of course',
+    'Anytime :sun_with_face: :full_moon_with_face:'
+  ])
+})
+
 // demonstrate returning an attachment...
 slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
   msg.say({
